7a62ba490e9fb4f5cdf196d2a302314235dfa40d
JoyceAja <ajagbe.joyce@gmail.com>
2022-10-04 23:31:26 -0400

    adding splash page

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync()
+  setTimeout(SplashScreen.hideAsync, 3000)
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
