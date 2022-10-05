75a2bfe7dda07a4016baf21fd99c788e94fab3cb
JoyceAja <ajagbe.joyce@gmail.com>
2022-10-05 00:08:38 -0400

    updating the login path

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
 
-import LoginPage from './CreateAccountPage'
+import LoginPage from './LoginPage'
 
 export default function App() {
   SplashScreen.preventAutoHideAsync()
